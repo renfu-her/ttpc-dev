419dd9bae1d2ca6f543a1813806612ac523193a8
Patrick Her <renfu.her@gmail.com>
2024-09-23 10:53:21 +0800

    chore: Remove unnecessary files and directories

--- mailer/routes/api.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\SendMessageController;
-Route::post('/send-message', [SendMessageController::class, 'sendMessage'])->name('send-message');
